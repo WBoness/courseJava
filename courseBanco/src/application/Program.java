a7f6c2625d597801647f7d6b080783405793c9d6
wboness <wbonesss@gmail.com>
2019-03-18 10:01:17 -0300

    Adicionando projetos da máquina de casa

---
+++ courseBanco/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+/* 1. Em um banco, para se cadastrar uma conta bancária, é necessário informar 
+ *    o número da conta, o nome do titular da conta, e o valor de depósito inicial 
+ *    que o titular depositou ao abrir a conta.
+ *    Este valor de depósito inicial, entretanto, é opcional, ou seja: 
+ *    se o titular não tiver dinheiro a depositar no momento de abrir sua
+ *    conta, o depósito inicial não será feito e o saldo inicial da conta 
+ *    será, naturalmente, zero.
+ *     - Importante: uma vez que uma conta bancária foi aberta, o número da conta 
+ *       nunca poderá ser alterado.
+ *     - O nome do titular pode ser alterado (pois uma pessoa pode mudar de nome 
+ *       por ocasião de casamento, por exemplo).
+ *       
+ * 
+ */
+
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("funcionando!");
+
+	}
+
+}
