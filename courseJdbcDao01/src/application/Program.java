e156d78a9d9fe21f3b7cf89683924e70f226b090
wboness <wbonesss@gmail.com>
2019-04-11 15:42:44 -0300

    JDBC DAO Criação Classe Department

---
+++ courseJdbcDao01/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+
+		Department depto = new Department(1,"Books");// testando classe Departamento
+		System.out.println(depto);
+
+	}
+
+}
