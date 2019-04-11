1171c988478ebffd0039fd8165e083bd7b7df195
wboness <wbonesss@gmail.com>
2019-04-11 15:56:18 -0300

    JDBC DAO Criação Class Seller

--- courseJdbcDao01/src/application/Program.java
+++ courseJdbcDao01/src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,9 @@ public class Program {
 
 		Department depto = new Department(1,"Books");// testando classe Departamento
 		System.out.println(depto);
+		
+		Seller vendor = new Seller(1, "Wellington Boness", "wboness@bol.com.br", new Date(), 3000.0, depto);
+		System.out.println(vendor);
 
 	}
 
