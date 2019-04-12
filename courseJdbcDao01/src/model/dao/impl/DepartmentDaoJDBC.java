2063893e4378c4a14ae43920eacfe99feaa9c2e3
wboness <wbonesss@gmail.com>
2019-04-11 23:53:27 -0300

    JDBC DAO DepartmentDaoJDBC.Update

--- courseJdbcDao01/src/model/dao/impl/DepartmentDaoJDBC.java
+++ courseJdbcDao01/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -52,7 +52,22 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department depto) {
-		
+
+PreparedStatement st=null;
+		try {
+			st=conn.prepareStatement("UPDATE Department "
+						+ "SET Name = ? "
+						+ "WHERE Id = ?");
+			st.setString(1, depto.getNome());
+			st.setInt(2, depto.getId());
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
