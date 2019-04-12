70278996e91c52c76630f598aac4deeb44a27f8b
wboness <wbonesss@gmail.com>
2019-04-11 23:51:44 -0300

    JDBC DAO DepartmentDaoJDBC.findAll

--- courseJdbcDao01/src/model/dao/impl/DepartmentDaoJDBC.java
+++ courseJdbcDao01/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -22,56 +22,17 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	
 	@Override
 	public void insert(Department depto) {
-		PreparedStatement st = null;
-		try {
-			st=conn.prepareStatement("INSERT INTO Department"
-					+ "(Name) VALUES (?)",
-						Statement.RETURN_GENERATED_KEYS);
-			st.setString(1, depto.getNome());
-			int rowsAffeted = st.executeUpdate();
-			
-			if (rowsAffeted>0) {
-				ResultSet rs = st.getGeneratedKeys();
-				if (rs.next()) {
-					int id=rs.getInt(1);
-					depto.setId(id);
-				}
-				DB.closeResultSet(rs);
-			}
-			else {
-				throw new DbException("Erro Inesperado! Nenhuma inserção!");
-			}
-		}
-		catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-		}
+		
 	}
 
 	@Override
 	public void update(Department depto) {
-		PreparedStatement st=null;
-		try {
-			st=conn.prepareStatement("UPDATE Department "
-						+ "SET Name = ? "
-						+ "WHERE Id = ?");
-			st.setString(1, depto.getNome());
-			st.setInt(2, depto.getId());
-			st.executeUpdate();
-		}
-		catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-		}
+		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
