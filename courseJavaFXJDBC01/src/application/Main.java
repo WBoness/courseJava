ba5215c745719e94301e5d3ba36474e077a4b014
wboness <wbonesss@gmail.com>
2019-04-13 15:10:28 -0300

    Projeto JavaFXJDBC - AboutView e inserção no VBox

--- courseJavaFXJDBC01/src/application/Main.java
+++ courseJavaFXJDBC01/src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		//Agora está instanciando ao inves de invocar o método estático
@@ -20,8 +23,12 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);//ajusta o scrollpane à altura
 			scrollPane.setFitToWidth(true);//ajusta à largura
 			
+			/* Alteração após cria variável estática mainScene
 			Scene mainScene = new Scene(scrollPane); //cria a cena principal (passando objeto principal
 												 // da view - AnchorPane vazio
+   		  */
+			mainScene = new Scene(scrollPane);
+			
 			primaryStage.setScene(mainScene);// palco da sena, seta o palco
 			primaryStage.setTitle("Exemplo de Aplicação JavaFX");// coloca título
 			primaryStage.show();
@@ -32,5 +39,9 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 }
--- courseJavaFXJDBC01/src/gui/MainViewController.java
+++ courseJavaFXJDBC01/src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -29,10 +38,49 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("Ação do Item de menu About");
+		loadView("/gui/AboutView.fxml");
 	}
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 
+	}
+	
+	private void loadView (String absoluteName) {//carrega uma view
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			/*
+			 * 0.View carregada;  
+			 * 1.Tem que mostrar a view dentro da janela principal;
+			 * 		-para trabalhar com a janela principal:
+			 * 			1. Tem que ter uma referência da cena em que a janela principal está (Main.java)
+			 * 			2. Ela está dentro do primaryStage (palco)
+			 * 			3. tem que guardar e referenciar a cena (variável Scene estática mainScene)
+			 * 				- cria um getMainScene() para obter a cena principal
+			 * 		- precisa da referência porque precisa preservara cena padrão e montar outra para ser exposta
+			 * 			1. armazena a cena
+			 * 			2. localiza o ponto do VBox da janela principal
+			 * 			3. limpa o conteúdo (com o original já preservado)
+			 * 			4. limpa o conteúdo
+			 * 			5. insere o conteúdo + o conteúdo do VBox do About
+			 * 
+			 */
+			
+			Scene mainScene = Main.getMainScene();//armazena a cena
+			/*ScrollPane mainScrollPane = new ScrollPane(mainScene.getRoot());//obtem o primeiro conteúdo da Janela principal
+			VBox mainVbox = (VBox) mainScrollPane.getContent();//obtem a referência para o VBox dentro do ScrollPane (com casting)
+			*/
+			//pode ser substituido por: 
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+						
+			// preservar o menuBar > limbar o VBox > incluir todo o conteúdo novamente
+			Node mainMenu = mainVBox.getChildren().get(0);//preserva o menuBar
+			mainVBox.getChildren().clear(); //Exclui os filhos do mainVBox
+			mainVBox.getChildren().add(mainMenu); //incluir o menubar +
+			mainVBox.getChildren().addAll(newVBox.getChildren()); // filhos do VBox da janela About
+		}
+		catch (IOException e) {
+			Alerts.showAlert("Erro carregando a página", null,e.getMessage() , AlertType.ERROR);
+		}
 	}
 }
