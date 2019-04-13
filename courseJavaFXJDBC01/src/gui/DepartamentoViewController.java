37ed931b2772fae3d874d9bf2869977d79e037bf
wboness <wbonesss@gmail.com>
2019-04-13 19:12:45 -0300

    Projeto JavaFXJDBC - Departamento - DepartamentoViewControler - campos e tamanho do TableView

--- courseJavaFXJDBC01/src/gui/DepartamentoViewController.java
+++ courseJavaFXJDBC01/src/gui/DepartamentoViewController.java
@@ -3,11 +3,14 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.entities.Departamento;
 
 public class DepartamentoViewController implements Initializable {
@@ -33,6 +36,17 @@ public class DepartamentoViewController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+		inicializaNodes(); //inicia algum componente na tela
+	}
+
+	//vai iniciar o comportamento das colunas 
+	private void inicializaNodes() {
+		tableColumIdDepartamento.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumIdDepartamento.setCellValueFactory(new PropertyValueFactory<>("nome"));
+		
+		//configurar altura e largura do tableView
+		Stage stage = (Stage) Main.getMainScene().getWindow(); //window é superclasse do stage (faz downcasting)
+		tableViewDepartamento.prefHeightProperty().bind(stage.heightProperty());
 		
 	}
 
